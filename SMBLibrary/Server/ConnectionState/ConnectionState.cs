cef7a6dc2967e015c6749252cb5dc5b582dbe84e
Tal Aloni <tal.aloni.il@gmail.com>
2017-03-04 11:13:11 +0200

    Moved SMBDialect to a separate class

--- SMBLibrary/Server/ConnectionState/ConnectionState.cs
+++ SMBLibrary/Server/ConnectionState/ConnectionState.cs
@@ -15,14 +15,6 @@ namespace SMBLibrary.Server
 {
     public delegate void LogDelegate(Severity severity, string message);
 
-    public enum SMBDialect
-    {
-        NotSet,
-        NTLM012, // NT LM 0.12
-        SMB202,  // SMB 2.0.2
-        SMB210,  // SMB 2.1
-    }
-
     public class ConnectionState
     {
         public Socket ClientSocket;
