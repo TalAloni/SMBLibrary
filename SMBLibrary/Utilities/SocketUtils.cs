eff1f08ab881cfeeb574dd332aaf4f42e6c2c450
Tal Aloni <tal.aloni.il@gmail.com>
2017-03-07 10:35:50 +0200

    SocketUtils.ReleaseSocket: Improved exception handling

--- SMBLibrary/Utilities/SocketUtils.cs
+++ SMBLibrary/Utilities/SocketUtils.cs
@@ -1,3 +1,9 @@
+/* Copyright (C) 2012-2017 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+ * 
+ * You can redistribute this program and/or modify it under the terms of
+ * the GNU Lesser Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ */
 using System;
 using System.Collections.Generic;
 using System.Net;
@@ -36,10 +42,14 @@ namespace Utilities
             {
                 if (socket.Connected)
                 {
-                    socket.Shutdown(SocketShutdown.Both);
                     try
                     {
+                        socket.Shutdown(SocketShutdown.Both);
                         socket.Disconnect(false);
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        return;
                     }
                     catch (SocketException)
                     { }
