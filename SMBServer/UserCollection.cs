f9c77ff6a050ed4ffbc92af6dba60b19db4bf9c6
Tal Aloni <tal.aloni.il@gmail.com>
2017-03-10 15:47:47 +0200

    User / UserCollection: Changed namespace to SMBServer

--- SMBServer/UserCollection.cs
+++ SMBServer/UserCollection.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2017 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -6,9 +6,8 @@
  */
 using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace SMBLibrary.Server
+namespace SMBServer
 {
     public class UserCollection : List<User>
     {
