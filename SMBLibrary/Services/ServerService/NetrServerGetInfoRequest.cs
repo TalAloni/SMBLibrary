536d68a620dba7465dbece07129d1d76080b6f7b
Tal Aloni <tal.aloni.il@gmail.com>
2017-09-09 11:16:48 +0300

    ServerService: NetrServerGetInfo: Added constructor

--- SMBLibrary/Services/ServerService/NetrServerGetInfoRequest.cs
+++ SMBLibrary/Services/ServerService/NetrServerGetInfoRequest.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2017 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -19,6 +19,10 @@ namespace SMBLibrary.Services
     {
         public string ServerName;
         public uint Level;
+
+        public NetrServerGetInfoRequest()
+        {
+        }
 
         public NetrServerGetInfoRequest(byte[] buffer)
         {
