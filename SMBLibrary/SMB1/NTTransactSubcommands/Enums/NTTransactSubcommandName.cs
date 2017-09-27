420c2264d0411f8036a70eea5b2c3ce1085c926d
Tal Aloni <tal.aloni.il@gmail.com>
2017-09-27 14:17:02 +0300

    SMB1: NTTransactSubcommandName: Added NT_TRANSACT_QUERY_QUOTA and NT_TRANSACT_SET_QUOTA

--- SMBLibrary/SMB1/NTTransactSubcommands/Enums/NTTransactSubcommandName.cs
+++ SMBLibrary/SMB1/NTTransactSubcommands/Enums/NTTransactSubcommandName.cs
@@ -2,7 +2,7 @@
 namespace SMBLibrary.SMB1
 {
     /// <summary>
-    /// This is the Function field in NTTransactRequest
+    /// This is the Function field in SMB_COM_NT_TRANSACT request
     /// </summary>
     public enum NTTransactSubcommandName : ushort
     {
@@ -12,5 +12,7 @@ namespace SMBLibrary.SMB1
         NT_TRANSACT_NOTIFY_CHANGE = 0x0004,
         // NT_TRANSACT_RENAME = 0x0005,
         NT_TRANSACT_QUERY_SECURITY_DESC = 0x0006,
+        NT_TRANSACT_QUERY_QUOTA = 0x0007,
+        NT_TRANSACT_SET_QUOTA = 0x0008,
     }
 }
