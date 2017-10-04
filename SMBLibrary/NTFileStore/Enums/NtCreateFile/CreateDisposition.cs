92d7a0531fae0d66bafad356ee6935a5d17d1c82
Tal Aloni <tal.aloni.il@gmail.com>
2017-10-04 16:11:05 +0300

    CreateDisposition: Updated documentation

--- SMBLibrary/NTFileStore/Enums/NtCreateFile/CreateDisposition.cs
+++ SMBLibrary/NTFileStore/Enums/NtCreateFile/CreateDisposition.cs
@@ -4,41 +4,38 @@ namespace SMBLibrary
     public enum CreateDisposition : uint
     {
         /// <summary>
-        /// If the file already exists, it SHOULD be superseded (overwritten).
-        /// If it does not already exist, then it SHOULD be created.
+        /// If the file already exists, replace it with the given file.
+        /// If it does not, create the given file.
         /// </summary>
         FILE_SUPERSEDE = 0x0000,
 
         /// <summary>
-        /// If the file already exists, it SHOULD be opened rather than created.
-        /// If the file does not already exist, the operation MUST fail.
+        /// If the file already exists, open it [instead of creating a new file].
+        /// If it does not, fail the request [and do not create a new file].
         /// </summary>
         FILE_OPEN = 0x0001,
         
         /// <summary>
-        /// If the file already exists, the operation MUST fail.
-        /// If the file does not already exist, it SHOULD be created.
+        /// If the file already exists, fail the request [and do not create or open the given file].
+        /// If it does not, create the given file.
         /// </summary>
         FILE_CREATE = 0x0002,
         
         /// <summary>
-        /// If the file already exists, it SHOULD be opened.
-        /// If the file does not already exist, then it SHOULD be created.
-        /// This value is equivalent to (FILE_OPEN | FILE_CREATE).
+        /// If the file already exists, open it.
+        /// If it does not, create the given file.
         /// </summary>
         FILE_OPEN_IF = 0x0003,
 
         /// <summary>
-        /// If the file already exists, it SHOULD be opened and truncated.
-        /// If the file does not already exist, the operation MUST fail.
-        /// The client MUST open the file with at least GENERIC_WRITE access for the command to succeed.
+        /// If the file already exists, open it and overwrite it.
+        /// If it does not, fail the request.
         /// </summary>
         FILE_OVERWRITE = 0x0004,
         
         /// <summary>
-        /// If the file already exists, it SHOULD be opened and truncated.
-        /// If the file does not already exist, it SHOULD be created.
-        /// The client MUST open the file with at least GENERIC_WRITE access.
+        /// If the file already exists, open it and overwrite it.
+        /// If it does not, create the given file.
         /// </summary>
         FILE_OVERWRITE_IF = 0x0005,
     }
