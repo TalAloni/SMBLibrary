c024168e12619a8ce9d5d55e1048493165733f4e
Tal Aloni <tal.aloni.il@gmail.com>
2018-05-17 13:42:42 +0300

    RPC: FaultPDU parameterless constructor will now set Data to an empty byte array

--- SMBLibrary/RPC/PDU/FaultPDU.cs
+++ SMBLibrary/RPC/PDU/FaultPDU.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014-2017 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2018 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -30,6 +30,7 @@ namespace SMBLibrary.RPC
         public FaultPDU() : base()
         {
             PacketType = PacketTypeName.Fault;
+            Data = new byte[0];
             AuthVerifier = new byte[0];
         }
 
