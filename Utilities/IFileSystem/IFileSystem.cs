6cab13c1c9572603deb4b5d0cf4207977a33afba
Tal Aloni <tal.aloni.il@gmail.com>
2018-12-11 11:21:19 +0200

    IFileSystem: Removed ArgumentException from documented list of exceptions that implementations are expected to use

--- Utilities/IFileSystem/IFileSystem.cs
+++ Utilities/IFileSystem/IFileSystem.cs
@@ -6,66 +6,56 @@ namespace Utilities
 {
     public interface IFileSystem
     {
-        /// <exception cref="System.ArgumentException"></exception>
         /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
         /// <exception cref="System.IO.FileNotFoundException"></exception>
         /// <exception cref="System.IO.IOException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
         FileSystemEntry GetEntry(string path);
 
-        /// <exception cref="System.ArgumentException"></exception>
         /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
         /// <exception cref="System.IO.IOException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
         FileSystemEntry CreateFile(string path);
 
-        /// <exception cref="System.ArgumentException"></exception>
         /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
         /// <exception cref="System.IO.IOException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
         FileSystemEntry CreateDirectory(string path);
 
-        /// <exception cref="System.ArgumentException"></exception>
         /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
         /// <exception cref="System.IO.FileNotFoundException"></exception>
         /// <exception cref="System.IO.IOException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
         void Move(string source, string destination);
 
-        /// <exception cref="System.ArgumentException"></exception>
         /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
         /// <exception cref="System.IO.FileNotFoundException"></exception>
         /// <exception cref="System.IO.IOException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
         void Delete(string path);
 
-        /// <exception cref="System.ArgumentException"></exception>
         /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
         /// <exception cref="System.IO.IOException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
         List<FileSystemEntry> ListEntriesInDirectory(string path);
 
-        /// <exception cref="System.ArgumentException"></exception>
         /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
         /// <exception cref="System.IO.FileNotFoundException"></exception>
         /// <exception cref="System.IO.IOException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
         List<KeyValuePair<string, ulong>> ListDataStreams(string path);
 
-        /// <exception cref="System.ArgumentException"></exception>
         /// <exception cref="System.IO.DirectoryNotFoundException"></exception>
         /// <exception cref="System.IO.FileNotFoundException"></exception>
         /// <exception cref="System.IO.IOException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
         Stream OpenFile(string path, FileMode mode, FileAccess access, FileShare share, FileOptions options);
 
-        /// <exception cref="System.ArgumentException"></exception>
         /// <exception cref="System.IO.FileNotFoundException"></exception>
         /// <exception cref="System.IO.IOException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
         void SetAttributes(string path, bool? isHidden, bool? isReadonly, bool? isArchived);
 
-        /// <exception cref="System.ArgumentException"></exception>
         /// <exception cref="System.IO.FileNotFoundException"></exception>
         /// <exception cref="System.IO.IOException"></exception>
         /// <exception cref="System.UnauthorizedAccessException"></exception>
