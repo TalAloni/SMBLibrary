31a41440e0a2d6c1b3ccb42a3a3dfaf91d0a9b08
Tal Aloni <tal.aloni.il@gmail.com>
2019-01-23 23:52:58 +0200

    TransactionQueryNamedPipeStateResponse: Added ParametersLength const

--- SMBLibrary/SMB1/TransactionSubcommands/TransactionQueryNamedPipeStateResponse.cs
+++ SMBLibrary/SMB1/TransactionSubcommands/TransactionQueryNamedPipeStateResponse.cs
@@ -16,6 +16,8 @@ namespace SMBLibrary.SMB1
     /// </summary>
     public class TransactionQueryNamedPipeStateResponse : TransactionSubcommand
     {
+        public const int ParametersLength = 2;
+        // Parameters;
         public NamedPipeStatus NMPipeStatus;
 
         public override byte[] GetSetup()
