156e9a0631d557f290c77a3919b7cf28735b8ba0
Tal Aloni <tal.aloni.il@gmail.com>
2020-01-25 20:56:30 +0200

    NetBios: NameQueryRequest: Removed incorrect ARCount value from constructor

--- SMBLibrary/NetBios/NameServicePackets/NameQueryRequest.cs
+++ SMBLibrary/NetBios/NameServicePackets/NameQueryRequest.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2020 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -24,7 +24,6 @@ namespace SMBLibrary.NetBios
         {
             Header = new NameServicePacketHeader();
             Header.OpCode = NameServiceOperation.QueryRequest;
-            Header.ARCount = 1;
             Question = new QuestionSection();
         }
 
