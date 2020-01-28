ada1fc79372e11a08527ebaf747a780bf0d2c093
Tal Aloni <tal.aloni.il@gmail.com>
2020-01-28 20:44:26 +0200

    SMB Server v1.4.0

--- SMBServer/Properties/AssemblyInfo.cs
+++ SMBServer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tal Aloni")]
 [assembly: AssemblyProduct("SMBServer")]
-[assembly: AssemblyCopyright("Copyright © Tal Aloni 2014-2019")]
+[assembly: AssemblyCopyright("Copyright © Tal Aloni 2014-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.3.9.0")]
-[assembly: AssemblyFileVersion("1.3.9.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
