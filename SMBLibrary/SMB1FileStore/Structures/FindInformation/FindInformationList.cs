0f43ef1f88b2512a8780f7620252586728eef86e
TalAloni <tal.aloni.il@gmail.com>
2020-09-04 16:13:55 +0300

    Client: Bugfix: Reading FindInformationList may have caused infinite loop

--- SMBLibrary/SMB1FileStore/Structures/FindInformation/FindInformationList.cs
+++ SMBLibrary/SMB1FileStore/Structures/FindInformation/FindInformationList.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2020 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -24,6 +24,10 @@ namespace SMBLibrary.SMB1
             {
                 FindInformation entry = FindInformation.ReadEntry(buffer, offset, informationLevel, isUnicode);
                 this.Add(entry);
+                if (entry.NextEntryOffset == 0)
+                {
+                    break;
+                }
                 offset += (int)entry.NextEntryOffset;
             }
         }
