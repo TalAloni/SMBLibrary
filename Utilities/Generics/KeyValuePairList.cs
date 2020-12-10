349dcdd3200588823aa3971b26895d8c3ab8a180
TalAloni <tal.aloni.il@gmail.com>
2020-12-11 00:22:46 +0200

    Utilities: KeyValuePairList: Moved Sort logic to partial class

--- Utilities/Generics/KeyValuePairList.cs
+++ Utilities/Generics/KeyValuePairList.cs
@@ -4,9 +4,7 @@
  * the GNU Lesser Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
  */
-using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 
 namespace Utilities
 {
@@ -80,36 +78,6 @@ namespace Utilities
                 }
                 return result;
             }
-        }
-
-        new public void Sort()
-        {
-            this.Sort(Comparer<TKey>.Default);
-        }
-
-        public void Sort(ListSortDirection sortDirection)
-        {
-            Sort(Comparer<TKey>.Default, sortDirection);
-        }
-
-        public void Sort(IComparer<TKey> comparer, ListSortDirection sortDirection)
-        {
-            if (sortDirection == ListSortDirection.Ascending)
-            {
-                Sort(comparer);
-            }
-            else
-            {
-                Sort(new ReverseComparer<TKey>(comparer));
-            }
-        }
-
-        public void Sort(IComparer<TKey> comparer)
-        {
-            this.Sort(delegate(KeyValuePair<TKey, TValue> a, KeyValuePair<TKey, TValue> b)
-            {
-                return comparer.Compare(a.Key, b.Key);
-            });
         }
 
         public new KeyValuePairList<TKey, TValue> GetRange(int index, int count)
