4b3057440302b47262af51ee16c59ddcc183eb3a
Jeroen Savat <jeroen.savat@lansweeper.com>
2021-04-01 11:13:02 +0200

    fix: bump

--- SMBLibrary/Properties/AssemblyInfo.cs
+++ SMBLibrary/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembly
+// associated with an assembly.
 [assembly: AssemblyTitle("SMBLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
