d0aef4212cae56da2fcf87a3c6f1f9f162cc66f6
Jeroen Savat <jeroen.savat@lansweeper.com>
2021-04-01 09:30:33 +0200

    feature: added circleci build

---
+++ release.config.js
@@ -0,0 +1,42 @@
+const { version: previousVersion } = require("./package.json");
+const projectfolder = "SMBLibrary/";
+const csprojfile = "Lansweeper.SMBLibrary.csproj";
+const nugetpackage = "Lansweeper.SMBLibrary";
+
+module.exports = {
+  "branches": [
+    "main"
+  ],
+  "plugins": [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/exec",
+      {
+        "prepareCmd": `sed -i -E 's/<Version>${previousVersion}/<Version>\${nextRelease.version}/' ${projectfolder}${csprojfile} && \
+          git add ${projectfolder}${csprojfile} && \
+          git commit -m ":bookmark: Bump from ${previousVersion} to \${nextRelease.version} in ${projectfolder}${csprojfile}" && \
+          dotnet pack ${projectfolder}${csprojfile} --configuration Release && \
+          dotnet nuget push ./${projectfolder}bin/Release/${nugetpackage}.\${nextRelease.version}.nupkg --source \"github\" --api-key $GITHUB_TOKEN`
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": [
+          "CHANGELOG.md",
+          "package.json"
+        ],
+        "message": ":bookmark: Release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ],
+    "@semantic-release/github",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md"
+      }
+    ]
+  ]
+}
