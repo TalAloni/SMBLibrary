921b672437937e60e45b604d29ea4d1f32836e80
Tal Aloni <tal.aloni.il@gmail.com>
2022-10-28 14:12:32 +0300

    SMBServer 1.4.9

--- SMBServer/Properties/AssemblyInfo.cs
+++ SMBServer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.8.0")]
-[assembly: AssemblyFileVersion("1.4.8.0")]
+[assembly: AssemblyVersion("1.4.9.0")]
+[assembly: AssemblyFileVersion("1.4.9.0")]
