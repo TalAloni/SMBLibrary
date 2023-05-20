ecce6f9311655556d47a68f46e07fc096f26b1e0
Tal Aloni <tal.aloni.il@gmail.com>
2023-05-20 09:50:54 +0300

    SMBServer 1.5.0

--- SMBServer/Properties/AssemblyInfo.cs
+++ SMBServer/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.4.9.0")]
-[assembly: AssemblyFileVersion("1.4.9.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
