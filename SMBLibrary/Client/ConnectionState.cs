d6a149707649f545830268082334c91069a4fdac
Tal Aloni <tal.aloni.il@gmail.com>
2023-12-14 19:43:05 +0200

    Client: ConnectionState: Remove unused using statements

--- SMBLibrary/Client/ConnectionState.cs
+++ SMBLibrary/Client/ConnectionState.cs
@@ -1,16 +1,12 @@
-/* Copyright (C) 2017-2020 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2017-2023 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
  */
 using System;
-using System.Collections.Generic;
-using System.Net;
 using System.Net.Sockets;
 using SMBLibrary.NetBios;
-using SMBLibrary.SMB1;
-using Utilities;
 
 namespace SMBLibrary.Client
 {
