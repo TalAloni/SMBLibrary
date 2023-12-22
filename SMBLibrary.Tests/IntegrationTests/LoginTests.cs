989b0515270452f2b36d7cf9faab26846ac25ca6
Tal Aloni <tal.aloni.il@gmail.com>
2023-12-22 12:40:36 +0200

    Tests: Added LoginTests

---
+++ SMBLibrary.Tests/IntegrationTests/LoginTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SMBLibrary.Authentication.GSSAPI;
+using SMBLibrary.Authentication.NTLM;
+using SMBLibrary.Client;
+using SMBLibrary.Server;
+using System;
+using System.Net;
+
+namespace SMBLibrary.Tests.IntegrationTests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        private int m_serverPort;
+        private SMBServer m_server;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            m_serverPort = 1000 + new Random().Next(50000);
+            SMBShareCollection shares = new SMBShareCollection();
+            IGSSMechanism gssMechanism = new IndependentNTLMAuthenticationProvider((username) => "password");
+            GSSProvider gssProvider = new GSSProvider(gssMechanism);
+            m_server = new SMBServer(shares, gssProvider);
+            m_server.Start(IPAddress.Loopback, SMBTransportType.DirectTCPTransport, m_serverPort, false, true, false, null);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            m_server.Stop();
+        }
+
+        [TestMethod]
+        public void When_ValidCredentialsProvided_LoginSucceed()
+        {
+            // Arrange
+            SMB2Client client = new SMB2Client();
+            client.Connect(IPAddress.Loopback, SMBTransportType.DirectTCPTransport, m_serverPort, 5000);
+
+            // Act
+            NTStatus status = client.Login("", "John", "password");
+
+            // Assert
+            Assert.AreEqual(NTStatus.STATUS_SUCCESS, status);
+        }
+
+        [TestMethod]
+        public void When_InvalidCredentialsProvided_LoginFails()
+        {
+            // Arrange
+            SMB2Client client = new SMB2Client();
+            client.Connect(IPAddress.Loopback, SMBTransportType.DirectTCPTransport, m_serverPort, 5000);
+
+            // Act
+            NTStatus status = client.Login("", "John", "Password");
+
+            // Assert
+            Assert.AreEqual(NTStatus.STATUS_LOGON_FAILURE, status);
+        }
+    }
+}
