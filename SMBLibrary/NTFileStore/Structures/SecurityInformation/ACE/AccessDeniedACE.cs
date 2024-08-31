3f054348d8dfa23b7b308528c53905667b4ce32a
Tal Aloni <tal.aloni.il@gmail.com>
2024-08-31 12:27:59 +0300

    ACE: Moved ACE Header to the ACE abstract class

--- SMBLibrary/NTFileStore/Structures/SecurityInformation/ACE/AccessDeniedACE.cs
+++ SMBLibrary/NTFileStore/Structures/SecurityInformation/ACE/AccessDeniedACE.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright (C) 2014-2017 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+﻿/* Copyright (C) 2014-2024 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -12,7 +12,6 @@ namespace SMBLibrary
     {
         public const int FixedLength = 8;
 
-        public AceHeader Header;
         public AccessMask Mask;
         public SID Sid;
 
