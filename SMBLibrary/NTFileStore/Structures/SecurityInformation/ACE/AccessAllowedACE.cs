3c7588b5751ea26ede7b1687a17ad102b94c1cff
Tal Aloni <tal.aloni.il@gmail.com>
2024-08-31 12:27:10 +0300

    ACE: Removed unused using statements

--- SMBLibrary/NTFileStore/Structures/SecurityInformation/ACE/AccessAllowedACE.cs
+++ SMBLibrary/NTFileStore/Structures/SecurityInformation/ACE/AccessAllowedACE.cs
@@ -4,8 +4,6 @@
  * the GNU Lesser Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
  */
-using System;
-using System.Collections.Generic;
 using Utilities;
 
 namespace SMBLibrary
