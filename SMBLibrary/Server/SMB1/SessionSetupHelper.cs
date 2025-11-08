885886e4d38969d6f10e286d93922fea62df7dff
Tal Aloni <tal.aloni.il@gmail.com>
2025-11-08 14:40:04 +0200

    Server: SessionSetupHelper: Correctly handle NTLM v1 non-extended-session-security with LmChallengeResponse set to 0 bytes

--- SMBLibrary/Server/SMB1/SessionSetupHelper.cs
+++ SMBLibrary/Server/SMB1/SessionSetupHelper.cs
@@ -1,4 +1,4 @@
-/* Copyright (C) 2014-2017 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2025 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
@@ -168,7 +168,7 @@ namespace SMBLibrary.Server.SMB1
             {
                 authenticateMessage.NegotiateFlags |= NegotiateFlags.ExtendedSessionSecurity;
             }
-            else
+            else if (lmChallengeResponse.Length == 24) // NTLM v1 non-extended-session-security: if LmChallengeResponse is set to 0 bytes then LanManagerSessionKey is not applicable.
             {
                 authenticateMessage.NegotiateFlags |= NegotiateFlags.LanManagerSessionKey;
             }
