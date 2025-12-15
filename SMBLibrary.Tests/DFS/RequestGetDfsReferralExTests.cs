bc99e21b26b2a9ecbcb72466ecadf44bd7f785c5
Tal Aloni <tal.aloni.il@gmail.com>
2025-12-15 23:15:57 +0200

    DFS: Implement RequestGetDfsReferralEx

---
+++ SMBLibrary.Tests/DFS/RequestGetDfsReferralExTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SMBLibrary.DFS;
+
+namespace SMBLibrary.Tests.DFS
+{
+    [TestClass]
+    public class RequestGetDfsReferralExTests
+    {
+        [TestMethod]
+        public void ParseRequestGetDfsReferralEx()
+        {
+            byte[] buffer = new byte[]
+            {
+                0x04, 0x00, 0x01, 0x00, 0x56, 0x00, 0x00, 0x00, 0x22, 0x00, 0x5c, 0x00, 0x4c, 0x00, 0x41, 0x00,
+                0x42, 0x00, 0x2e, 0x00, 0x4c, 0x00, 0x4f, 0x00, 0x43, 0x00, 0x41, 0x00, 0x4c, 0x00, 0x5c, 0x00,
+                0x46, 0x00, 0x69, 0x00, 0x6c, 0x00, 0x65, 0x00, 0x73, 0x00, 0x00, 0x00, 0x30, 0x00, 0x44, 0x00,
+                0x65, 0x00, 0x66, 0x00, 0x61, 0x00, 0x75, 0x00, 0x6c, 0x00, 0x74, 0x00, 0x2d, 0x00, 0x46, 0x00,
+                0x69, 0x00, 0x72, 0x00, 0x73, 0x00, 0x74, 0x00, 0x2d, 0x00, 0x53, 0x00, 0x69, 0x00, 0x74, 0x00,
+                0x65, 0x00, 0x2d, 0x00, 0x4e, 0x00, 0x61, 0x00, 0x6d, 0x00, 0x65, 0x00, 0x00, 0x00, 0x00
+            };
+
+            RequestGetDfsReferralEx request = new RequestGetDfsReferralEx(buffer);
+            Assert.AreEqual(4, request.MaxReferralLevel);
+            Assert.AreEqual(RequestGetDfsReferralExFlags.SiteName, request.Flags);
+            Assert.AreEqual(@"\LAB.LOCAL\Files", request.RequestFileName);
+            Assert.AreEqual("Default-First-Site-Name", request.SiteName);
+        }
+
+        [TestMethod]
+        public void Parse_RequestGetDfsReferralEx_GetBytes()
+        {
+            RequestGetDfsReferralEx request = new RequestGetDfsReferralEx();
+            request.MaxReferralLevel = 4;
+            request.Flags = RequestGetDfsReferralExFlags.SiteName;
+            request.RequestFileName = @"\LAB.LOCAL\Files";
+            request.SiteName = @"Default-First-Site-Name";
+
+            request = new RequestGetDfsReferralEx(request.GetBytes());
+            Assert.AreEqual(4, request.MaxReferralLevel);
+            Assert.AreEqual(RequestGetDfsReferralExFlags.SiteName, request.Flags);
+            Assert.AreEqual(@"\LAB.LOCAL\Files", request.RequestFileName);
+            Assert.AreEqual("Default-First-Site-Name", request.SiteName);
+        }
+    }
+}
---
+++ SMBLibrary/DFS/RequestGetDfsReferralEx.cs
@@ -0,0 +1,65 @@
+﻿/* Copyright (C) 2025 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+ * 
+ * You can redistribute this program and/or modify it under the terms of
+ * the GNU Lesser Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ */
+using Utilities;
+
+namespace SMBLibrary.DFS
+{
+    /// <summary>
+    /// [MS-DFSC] 2.2.3 REQ_GET_DFS_REFERRAL_EX
+    /// Extended DFS referral request that supports site-aware referrals.
+    /// </summary>
+    public class RequestGetDfsReferralEx
+    {
+        public ushort MaxReferralLevel;
+        public RequestGetDfsReferralExFlags Flags;
+        public string RequestFileName; // Unicode
+        public string SiteName;        // Optional, Unicode, null-terminated (when flag is set)
+
+        public RequestGetDfsReferralEx()
+        {
+        }
+
+        public RequestGetDfsReferralEx(byte[] buffer)
+        {
+            MaxReferralLevel = LittleEndianConverter.ToUInt16(buffer, 0);
+            Flags = (RequestGetDfsReferralExFlags)LittleEndianConverter.ToUInt16(buffer, 2);
+            uint requestDataLength = LittleEndianConverter.ToUInt32(buffer, 4);
+            ushort requestFileNameLength = LittleEndianConverter.ToUInt16(buffer, 8);
+            int dataOffset = 10;
+            RequestFileName = ByteReader.ReadNullTerminatedUTF16String(buffer, ref dataOffset);
+            if ((Flags & RequestGetDfsReferralExFlags.SiteName) != 0)
+            {
+                ushort siteNameLength = LittleEndianReader.ReadUInt16(buffer, ref dataOffset);
+                SiteName = ByteReader.ReadNullTerminatedUTF16String(buffer, ref dataOffset);
+            }
+        }
+
+        public byte[] GetBytes()
+        {
+            int length = 10 + RequestFileName.Length * 2 + 2;
+            if ((Flags & RequestGetDfsReferralExFlags.SiteName) != 0)
+            {
+                length += 2 + SiteName.Length * 2 + 2;
+            }
+
+            byte[] buffer = new byte[length];
+            LittleEndianWriter.WriteUInt16(buffer, 0, MaxReferralLevel);
+            LittleEndianWriter.WriteUInt16(buffer, 2, (ushort)Flags);
+            LittleEndianWriter.WriteUInt32(buffer, 4, (uint)(length - 8));
+            LittleEndianWriter.WriteUInt16(buffer, 8, (ushort)RequestFileName.Length);
+            int dataOffset = 10;
+            ByteWriter.WriteNullTerminatedUTF16String(buffer, ref dataOffset, RequestFileName);
+            if ((Flags & RequestGetDfsReferralExFlags.SiteName) != 0)
+            {
+                LittleEndianWriter.WriteUInt16(buffer, ref dataOffset, (ushort)SiteName.Length);
+                ByteWriter.WriteNullTerminatedUTF16String(buffer, ref dataOffset, SiteName);
+            }
+
+            return buffer;
+        }
+    }
+}
