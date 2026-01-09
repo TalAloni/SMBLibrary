b349a07015ff1aff97cbfc2d690d0d26b07bb397
Tal Aloni <tal.aloni.il@gmail.com>
2026-01-09 10:54:00 +0200

    NTDirectoryFileSystemTests.TestCancel: Skip test when platform is not Windows

--- SMBLibrary.Tests/NTFileStore/NTDirectoryFileSystemTests.cs
+++ SMBLibrary.Tests/NTFileStore/NTDirectoryFileSystemTests.cs
@@ -1,10 +1,11 @@
-/* Copyright (C) 2019-2024 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2019-2025 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
  */
 using System.IO;
+using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SMBLibrary.Win32;
 
@@ -25,6 +26,17 @@ namespace SMBLibrary.Tests
 
         public NTDirectoryFileSystemTests() : base(new NTDirectoryFileSystem(TestDirectoryPath))
         {
+        }
+
+        [TestMethod]
+        public override void TestCancel()
+        {
+            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                Assert.Inconclusive();
+            }
+
+            base.TestCancel();
         }
     }
 }
