7d4b66bda1c74ce45c343f3c3ca3b9e9f54dc310
Tal Aloni <tal.aloni.il@gmail.com>
2026-01-12 22:11:14 +0200

    SMB1: FileStoreResponseHelper: Remove unused using statements

--- SMBLibrary/Server/SMB1/FileStoreResponseHelper.cs
+++ SMBLibrary/Server/SMB1/FileStoreResponseHelper.cs
@@ -1,13 +1,10 @@
-/* Copyright (C) 2014-2017 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
+/* Copyright (C) 2014-2025 Tal Aloni <tal.aloni.il@gmail.com>. All rights reserved.
  * 
  * You can redistribute this program and/or modify it under the terms of
  * the GNU Lesser Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
  */
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Text;
 using SMBLibrary.SMB1;
 using Utilities;
 
