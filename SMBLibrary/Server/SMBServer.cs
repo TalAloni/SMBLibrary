96b7ff27da5acaf5079a23da422afad75daee7b5
Logan Stromberg <loganstromberg@gmail.com>
2026-02-19 13:36:33 -0800

    Expose port-overloaded SMBServer.Start() method to public
    Expose port-overloaded SMBServer.Start() method to public

--- SMBLibrary/Server/SMBServer.cs
+++ SMBLibrary/Server/SMBServer.cs
@@ -84,7 +84,7 @@ namespace SMBLibrary.Server
             Start(serverAddress, transport, port, enableSMB1, enableSMB2, enableSMB3, connectionInactivityTimeout);
         }
 
-        protected internal void Start(IPAddress serverAddress, SMBTransportType transport, int port, bool enableSMB1, bool enableSMB2, bool enableSMB3, TimeSpan? connectionInactivityTimeout)
+        public void Start(IPAddress serverAddress, SMBTransportType transport, int port, bool enableSMB1, bool enableSMB2, bool enableSMB3, TimeSpan? connectionInactivityTimeout)
         {
             if (!m_listening)
             {
@@ -490,3 +490,4 @@ namespace SMBLibrary.Server
         }
     }
 }
+
