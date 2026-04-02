c8225756b085258de9e602b91cda45d72c4dc503
aled <aled@wibblr.com>
2026-04-02 18:20:42 +0100

    Get different port numbers for tests that are run at the same instant in time.

--- SMBLibrary.Tests/Client/SMB2ClientTests.cs
+++ SMBLibrary.Tests/Client/SMB2ClientTests.cs
@@ -10,6 +10,7 @@ using System;
 using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Threading;
 
 namespace SMBLibrary.Tests.Client
@@ -20,11 +21,40 @@ namespace SMBLibrary.Tests.Client
         private int m_serverPort;
         private TcpListener m_tcpListener;
         private bool m_clientConnected;
+        
+        // ensure that multiple tests run at the same instant in time receive different port numbers
+        // by using a random number generator that is NOT seeded solely by current time
+        private RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+        
+        private int GetRandomPortNumber(int lowerBound, int upperBound)
+        {
+            if (lowerBound < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(lowerBound), "lowerBound must be greater than 0");
+            }
+            
+            if (upperBound < lowerBound || upperBound > 65535)
+            {
+                throw new ArgumentOutOfRangeException(nameof(upperBound), "upperBound must be greater-or-equal to lowerBound and less than 65536");
+            }
+            
+            // rejection sampling is overkill for this application, but whatever, at least the tests pass now.
+            // use 2 bytes to generate a random number in the range 0 to 65535
+            var bytes = new byte[2];
+            int portNumber;
+            do
+            {
+                rng.GetBytes(bytes);
+                portNumber = BitConverter.ToUInt16(bytes, 0);
+            } while (portNumber < lowerBound || portNumber > upperBound);
 
+            return portNumber;
+        }
+        
         [TestInitialize]
         public void Initialize()
         {
-            m_serverPort = 1000 + new Random().Next(50000);
+            m_serverPort = GetRandomPortNumber(1025, 65535);
             m_tcpListener = new TcpListener(IPAddress.Loopback, m_serverPort);
             m_tcpListener.Start();
         }
