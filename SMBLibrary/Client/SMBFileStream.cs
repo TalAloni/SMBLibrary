1e5f21f0cac09de96f2602bd0c7aea24b17366b1
VengefulWhitetail <vw4github@abine.org>
2026-08-01 14:11:13 -0400

    Added doc comment for access argument.

--- SMBLibrary/Client/SMBFileStream.cs
+++ SMBLibrary/Client/SMBFileStream.cs
@@ -87,7 +87,7 @@ namespace SMBLibrary.Client
         /// <param name="store">TODO</param>
         /// <param name="path">A relative path to the file that the stream will encapsulate.</param>
         /// <param name="fileMode">One of the enumeration values that determines how to open or create the file.</param>
-        /// <param name="fileAccess">TODO Default: <see cref="FileAccess.ReadWrite"/></param>
+        /// <param name="fileAccess">A bitwise combination of the enumeration values that determines how the file can be accessed by the stream. Default: <see cref="FileAccess.ReadWrite"/></param>
         /// <param name="fileShare">TODO Default: <see cref="FileShare.Read"/></param>
         /// <param name="fileOptions">A bitwise combination of the enumeration values that specifies additional file options. Default: <see cref="FileOptions.None"/></param>
         /// <param name="ownsStore">True to disconnect <paramref name="store"/> when the stream is disposed; otherwise, false. Default: false</param>
